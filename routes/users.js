1fbb7cc376ee51c78947ec928101b080857de94a
Maya Sasson <mayokit22@gmail.com>
2021-06-07 14:31:30 -0700

    fix: now the query works

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,20 @@ const router = express.Router();
 
 const client = require("./../db");
 
+const errorStatusCode = 500;
+
 /* GET users listing. */
 router.get('/validateUser', (req, res, next) => {
-  const { username, password } = req.params;
+  const { username, password } = req.query;
 
+  if (!username || !password) {
+    res.status(errorStatusCode).send('values werent sent')
+  }
+  const query = `SELECT id FROM activity_user WHERE user_name='${username}' AND password='${password}'`;
   client
-    .query(`SELECT id FROM activity_user WHERE username=${username} AND password=${password}`)
+    .query(query)
     .then(result => res.send(result.rowCount > 0))
-    .catch(error => res.status(500).send(error))
+    .catch(error => res.status(errorStatusCode).send(error))
 });
 
 module.exports = router;
