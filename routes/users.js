d1259f2ec36dfb5ad2095b152d3fa7719aefc32d
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 19:50:08 +0300

    feat(login req): created req

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const client = require("./../db");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource 2');
+router.get('/validateUser', (req, res, next) => {
+  const { username, password } = req.params;
+
+  client
+    .query(`SELECT id FROM activity_user WHERE username=${username} AND password=${password}`)
+    .then(result => res.send(result.rowCount > 0))
+    .catch(error => res.status(500).send(error))
 });
 
 module.exports = router;
