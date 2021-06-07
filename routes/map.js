0e27620ce99a13fcaa858f2948c0b6f250662840
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 13:08:34 +0300

    fix (map.js): fix connection to db

--- routes/map.js
+++ routes/map.js
@@ -1,32 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const { Client } = require('pg')
-const client = require('./../db')
+const client = require("./../db");
 
-const kidnappingEvents = "SELECT * FROM kidnapping_event" ;
-const accidentEvents = "SELECT * FROM accident_event" ;
-const shootingEvents = "SELECT * FROM shooting_event" ;
-const stabbingEvents = "SELECT * FROM stabbing_event" ;
+const kidnappingEvents = "SELECT * FROM kidnapping_event";
+const accidentEvents = "SELECT * FROM accident_event";
+const shootingEvents = "SELECT * FROM shooting_event";
+const stabbingEvents = "SELECT * FROM stabbing_event";
 
-router.get("/allEvents", function(req, res){
-  client.query(kidnappingEvents, function (err, result) {
+router.get("/kidnappingEvents", function(req, res) {
+  client.query(kidnappingEvents, function(err, result) {
     if (err) {
       console.log(err.message);
       throw err;
     }
-    res.send(result);
-  })
-})
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send(result.rows);
+  });
 });
 
-
-// router.post('/newevent', function(req, res, next) {
-//   req.
-// })
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
 module.exports = router;
