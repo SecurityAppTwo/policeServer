6a26edc1c69fba64180765f1134a505c156ea10e
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 15:50:50 +0300

    Fix(reports): added get request

--- routes/reports.js
+++ routes/reports.js
@@ -1,16 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const client = require('./../db');
-const moment = require('moment')
+const client = require("./../db");
+const moment = require("moment");
 
-
-router.post('/add/kidnapEvent', function(req, res) {
-    var formatter= 'YYYY-MM-DD';
-    var start = new Date(req.body.date);
-    var end = new Date(req.body.reportDate);
-    var date = (moment(start).format(formatter));
-    var reportDate = (moment(end).format(formatter));
-    const addKidnapQuery = `INSERT INTO kidnapping_event (
+router.post("/add/kidnapEvent", function(req, res) {
+  var formatter = "YYYY-MM-DD";
+  var start = new Date(req.body.date);
+  var end = new Date(req.body.reportDate);
+  var date = moment(start).format(formatter);
+  var reportDate = moment(end).format(formatter);
+  const addKidnapQuery = `INSERT INTO kidnapping_event (
         kidnapper, kidnapped, last_location, date, reported_by, report_date, lon, lat, type
     )
     VALUES('${req.body.kidnapper}',
@@ -23,18 +22,17 @@ router.post('/add/kidnapEvent', function(req, res) {
         ${req.body.lat},
         'חטיפה'
         )`;
-    client
+  client
     .query(addKidnapQuery)
-    .then(() => res.send('Success'))
-    .catch(error => res.status(500)
-    .send(error? error.message : 'error'))
+    .then(() => res.send("Success"))
+    .catch(error => res.status(500).send(error ? error.message : "error"));
 });
 
-router.post('/add/stabbingEvent', function(req, res) {
-    var formatter= 'YYYY-MM-DD';
-    var start = new Date(req.body.date);
-    var date = (moment(start).format(formatter));
-    const addStabbingQuery = `INSERT INTO stabbing_event (
+router.post("/add/stabbingEvent", function(req, res) {
+  var formatter = "YYYY-MM-DD";
+  var start = new Date(req.body.date);
+  var date = moment(start).format(formatter);
+  const addStabbingQuery = `INSERT INTO stabbing_event (
         stabber, weapon_type, injured_count, date, reported_by, injured_type, lon, lat, type
     )
     VALUES('${req.body.stabber}',
@@ -46,21 +44,20 @@ router.post('/add/stabbingEvent', function(req, res) {
         ${req.body.lon},
         ${req.body.lat},
         'דקירה'
-        )`
-        client
+        )`;
+  client
     .query(addStabbingQuery)
-    .then(() => res.send('Success'))
-    .catch(error => res.status(500)
-    .send(error? error.message : 'error'))
+    .then(() => res.send("Success"))
+    .catch(error => res.status(500).send(error ? error.message : "error"));
 });
 
-router.post('/add/accidentEvent', function(req, res) {
-    var formatter= 'YYYY-MM-DD';
-    var start = new Date(req.body.date);
-    var end = new Date(req.body.reportDate);
-    var date = (moment(start).format(formatter));
-    var reportDate = (moment(end).format(formatter));
-    const addAccidentQuery = `INSERT INTO accident_event (
+router.post("/add/accidentEvent", function(req, res) {
+  var formatter = "YYYY-MM-DD";
+  var start = new Date(req.body.date);
+  var end = new Date(req.body.reportDate);
+  var date = moment(start).format(formatter);
+  var reportDate = moment(end).format(formatter);
+  const addAccidentQuery = `INSERT INTO accident_event (
         injured, driver, injured_count, date, reported_by, report_date, lon, lat, type
     )
     VALUES('${req.body.injured}',
@@ -72,19 +69,18 @@ router.post('/add/accidentEvent', function(req, res) {
         ${req.body.lon},
         ${req.body.lat},
         'תאונה'
-        )`
-        client
+        )`;
+  client
     .query(addAccidentQuery)
-    .then(() => res.send('Success'))
-    .catch(error => res.status(500)
-    .send(error? error.message : 'error'))
+    .then(() => res.send("Success"))
+    .catch(error => res.status(500).send(error ? error.message : "error"));
 });
 
-router.post('/add/shootingEvent', function(req, res) {
-    var formatter= 'YYYY-MM-DD';
-    var start = new Date(req.body.date);
-    var date = (moment(start).format(formatter));
-    const addShootingQuery = `INSERT INTO shooting_event (
+router.post("/add/shootingEvent", function(req, res) {
+  var formatter = "YYYY-MM-DD";
+  var start = new Date(req.body.date);
+  var date = moment(start).format(formatter);
+  const addShootingQuery = `INSERT INTO shooting_event (
         shooter, weapon_type, injured_count, date, reported_by, injured_type, lon, lat, type
     )
     VALUES('${req.body.shooter}',
@@ -96,11 +92,33 @@ router.post('/add/shootingEvent', function(req, res) {
         ${req.body.lon},
         ${req.body.lat},
         'ירי'
-        )`
-        client
+        )`;
+  client
     .query(addShootingQuery)
-    .then(() => res.send('Success'))
-    .catch(error => res.status(500)
-    .send(error? error.message : 'error'))
-})
+    .then(() => res.send("Success"))
+    .catch(error => res.status(500).send(error ? error.message : "error"));
+});
+
+const kidnappingEvents =
+  "SELECT type, last_location , date FROM kidnapping_event ORDER BY date";
+const accidentEvents =
+  "SELECT type, injured_count ,date FROM accident_event ORDER BY date";
+const shootingEvents =
+  "SELECT type, injured_count , date FROM shooting_event ORDER BY date";
+const stabbingEvents =
+  "SELECT type, injured_count , date FROM stabbing_event ORDER BY date";
+const allEvents = [
+  kidnappingEvents,
+  accidentEvents,
+  shootingEvents,
+  stabbingEvents
+];
+
+/*Get request that returns all the events with their dates and x,y coordinates */
+router.get("/allEventsReported", function(req, res) {
+  Promise.all(allEvents.map(query => client.query(query))).then(results =>
+    res.send(results.map(result => result.rows))
+  );
+});
+
 module.exports = router;
