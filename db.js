c369f85a99ec94829cf9556bb5af918585a03696
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 18:10:41 +0300

    db keys

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const db = new Client({
     user: 'admin',
-    host: '127.0.0.1',
+    host: '172.30.196.58',
     database: 'securityapptwodb',
     password: 'mtgand6',
-    port: 31000,
+    port: 5432,
 });
 
-db
-  .connect()
-  .then(() => console.log('connected'))
-  .catch(err => console.log('connection error', err.stack));
+db.connect()
+  .then(() => console.log("connected"))
+  .catch(err => console.log("connection error", err.stack));
 
-  module.exports = db;
+module.exports = db;
