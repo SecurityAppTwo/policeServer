797baedf6d44e8a279daba974bd2f5f3e3e16afa
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 10:26:14 +0300

    fix (map.js): fix post request

--- routes/map.js
+++ routes/map.js
@@ -20,47 +20,68 @@ router.get("/allEvents", function(req, res) {
   );
 });
 
-router.post("/addEvent", function(req, res) {
+router.post("/addEvent", async function(req, res) {
   const eventDetails = req.body;
   let insertQuery;
-
   switch (eventDetails.type) {
     case "accident_event":
       insertQuery = `
     INSERT INTO public.accident_event(
     injured, driver, injured_count, date, 
     reported_by, report_date, lon, lat, type)
-    VALUES (${eventDetails.injured}, ${eventDetails.driver}, ${eventDetails.injured_count}, ${eventDetails.date},
-       ${eventDetails.reported_by}, ${eventDetails.report_date}, ${eventDetails.lon}, ${eventDetails.lat}, ${eventDetails.type});`;
+    VALUES (${"'" + eventDetails.injured + "'"}, ${"'" +
+        eventDetails.driver +
+        "'"}, ${eventDetails.injured_count}, ${"'" + eventDetails.date + "'"},
+       ${eventDetails.reported_by}, ${"'" + eventDetails.report_date + "'"}, ${
+        eventDetails.lon
+      }, ${eventDetails.lat}, ${"'" + eventDetails.type + "'"});`;
+      await client.query(insertQuery);
+      res.end();
       break;
     case "kidnapping_event":
       insertQuery = `
-         INSERT INTO public.kidnapping_event(
-         kidnapper, kidnapped, last_location, date, reported_by, report_date, lon, lat, type)
-         VALUES (${eventDetails.kidnapper}, ${eventDetails.kidnapped}, ${eventDetails.last_location}, ${eventDetails.date},
-          ${eventDetails.reported_by}, ${eventDetails.report_date}, ${eventDetails.lon}, ${eventDetails.lat}, ${eventDetails.type});`;
+      INSERT INTO public.kidnapping_event(
+        kidnapper, kidnapped, last_location, date, reported_by, report_date, lon, lat, type)
+        VALUES (${"'" + eventDetails.kidnapper + "'"}, ${"'" +
+        eventDetails.kidnapped +
+        "'"}, ${"'" + eventDetails.last_location + "'"}, ${"'" +
+        eventDetails.date +
+        "'"},
+          ${eventDetails.reported_by}, ${"'" +
+        eventDetails.report_date +
+        "'"}, ${eventDetails.lon}, ${eventDetails.lat}, ${"'" +
+        eventDetails.type +
+        "'"});`;
+      await client.query(insertQuery);
+      res.end();
       break;
     case "shooting_event":
       insertQuery = `
       INSERT INTO public.shooting_event(
       shooter, weapon_type, injured_count, date, reported_by, injured_type, lon, lat, type)
-      VALUES (${eventDetails.shooter}, ${eventDetails.weapon_type}, ${eventDetails.injured_count}, ${eventDetails.date},
-        ${eventDetails.reported_by}, ${eventDetails.injured_type}, ${eventDetails.lon}, ${eventDetails.lat}, ${eventDetails.type});`;
+      VALUES (${"'" + eventDetails.shooter + "'"}, ${"'" +
+        eventDetails.weapon_type +
+        "'"}, ${eventDetails.injured_count}, ${"'" + eventDetails.date + "'"},
+        ${eventDetails.reported_by}, ${eventDetails.injured_type}, ${
+        eventDetails.lon
+      }, ${eventDetails.lat}, ${"'" + eventDetails.type + "'"});`;
+      await client.query(insertQuery);
+      res.end();
       break;
     case "stabbing_event":
       insertQuery = `
       INSERT INTO public.stabbing_event(
       stabber, weapon_type, injured_count, date, reported_by, injured_type, lon, lat, type)
-      VALUES (${eventDetails.stabber}, ${eventDetails.weapon_type}, ${eventDetails.injured_count}, ${eventDetails.date},
-        ${eventDetails.reported_by}, ${eventDetails.injured_type}, ${eventDetails.lon}, ${eventDetails.lat}, ${eventDetails.type});`;
+      VALUES (${"'" + eventDetails.stabber + "'"}, ${"'" +
+        eventDetails.weapon_type +
+        "'"}, ${eventDetails.injured_count}, ${"'" + eventDetails.date + "'"},
+        ${eventDetails.reported_by}, ${eventDetails.injured_type}, ${
+        eventDetails.lon
+      }, ${eventDetails.lat}, ${"'" + eventDetails.type + "'"});`;
+      await client.query(insertQuery);
+      res.end();
       break;
   }
-  client.query(insertQuery).then(result => res.send(result.rows));
-});
-
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
