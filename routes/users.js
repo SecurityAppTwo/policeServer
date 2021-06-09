2fda92583f3f9272ef7a063e58263a050d5c55de
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 11:17:26 +0300

    fixed api

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,22 @@ router.get('/validateUser', (req, res, next) => {
   if (!username || !password) {
     res.status(errorStatusCode).send('values werent sent')
   }
-  const query = `SELECT id FROM activity_user WHERE user_name='${username}' AND password='${password}'`;
+
+  const query = `SELECT id, is_cop FROM activity_user WHERE user_name='${username}' AND password='${password}'`;
   client
     .query(query)
-    .then(result => res.send({isValid: result.rowCount > 0}))
+    .then(result => {
+      console.log(result)
+      if (result.rowCount > 0){
+        if ((result.rows)[0].isCop){
+          res.send({isValid: true, id : (result.rows)[0].id})
+        } else{
+          res.send({isValid: false})
+        }
+      } else{
+        res.send({isValid: false})
+      }
+    })
     .catch(error => res.status(errorStatusCode).send(error))
 });
 
