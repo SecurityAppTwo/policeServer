39cab01ca6cf08b9c471d508219716af2b03720e
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 15:27:47 +0300

    faet (all files): add user router and fix map router

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const client = require("./../db");
@@ -6,17 +6,28 @@ const client = require("./../db");
 const errorStatusCode = 500;
 
 /* GET users listing. */
-router.get('/validateUser', (req, res, next) => {
+router.get("/validateUser", (req, res, next) => {
   const { username, password } = req.query;
 
   if (!username || !password) {
-    res.status(errorStatusCode).send('values werent sent')
+    res.status(errorStatusCode).send("values werent sent");
   }
-  const query = `SELECT id FROM activity_user WHERE user_name='${username}' AND password='${password}'`;
+
+  const query = `SELECT id, is_cop FROM activity_user WHERE user_name='${username}' AND password='${password}'`;
   client
     .query(query)
-    .then(result => res.send({isValid: result.rowCount > 0}))
-    .catch(error => res.status(errorStatusCode).send(error))
+    .then(result => {
+      if (result.rowCount > 0) {
+        res.send({
+          isValid: true,
+          isCop: result.rows[0].is_cop,
+          id: result.rows[0].id
+        });
+      } else {
+        res.send({ isValid: false });
+      }
+    })
+    .catch(error => res.status(errorStatusCode).send(error));
 });
 
 module.exports = router;
