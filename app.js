ba8901085ed9ec6ec4bddba2e76d1fcd2eac13e7
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 17:10:33 +0300

    fix: conflicts

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
-
-var mapRouter = require("./routes/map");
-var usersRouter = require("./routes/users");
-var deRouter = require("./routes/de");
-=======
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -26,7 +14,6 @@ const subscriptionRouter=sub.router;
 const sendEventsToAll=sub.sendEventsToAll;
 
 var deRouter = require('./routes/de');
->>>>>>> 59bfada6e29fd35e34643ec45a2cb6eece92d027
 
 var app = express();
 
@@ -41,11 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-<<<<<<< HEAD
-app.use("/map", mapRouter);
-app.use("/users", usersRouter);
-app.use("/de", deRouter);
-=======
 app.use(
     cors({
         origin: '*'
@@ -57,7 +39,6 @@ app.use('/map', mapRouter);
 app.use('/users', usersRouter);
 app.use('/',subscriptionRouter);
 app.use('/de', deRouter);
->>>>>>> 59bfada6e29fd35e34643ec45a2cb6eece92d027
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
