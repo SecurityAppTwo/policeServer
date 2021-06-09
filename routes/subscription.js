94eb14161e25ce9b76cc3a14840a6e073ed6be3f
dana6322 <85433691+dana6322@users.noreply.github.com>
2021-06-09 12:59:47 +0300

    Update subscription.js

--- routes/subscription.js
+++ routes/subscription.js
@@ -26,10 +26,10 @@ function eventsHandler(request, response, next) {
     };
 
     clients.push(newClient);
-//     request.on('close', () => {
-//         console.log(`${clientId} Connection closed`);
-//         clients = clients.filter(client => client.id !== clientId);
-//     });
+    request.on('close', () => {
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter(client => client.id !== clientId);
+    });
 
 }
 
